b346e3daa5a07d72920e6e2551db3d7700da4763
Sultan <hasenovsultanbek@gmail.com>
2021-04-28 21:29:25 +0600

    Added token for registration

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -75,8 +75,26 @@ namespace Booking.API.Controllers
             try
             {
                 // create user
-                await _userService.RegisterAsync(user, model.Password);
-                return Ok();
+                user = await _userService.RegisterAsync(user, model.Password);
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(7),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenString = tokenHandler.WriteToken(token);
+
+                // return basic user info and authentication token
+                return Ok(new
+                {
+                    token = tokenString
+                });
             }
             catch (AppException ex)
             {
