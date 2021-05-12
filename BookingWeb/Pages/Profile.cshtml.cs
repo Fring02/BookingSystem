9d62776188d249336f727c91cf2b9a5bca64c6d9
Magauiya Mazhit <m.mazhit@astanait.edu.kz>
2021-05-12 20:04:59 +0600

    Front-end: Profile page layout is end

--- BookingWeb/Pages/Profile.cshtml.cs
+++ BookingWeb/Pages/Profile.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookingWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,20 @@ namespace BookingWeb.Pages
 {
     public class ProfileModel : PageModel
     {
+
+        [BindProperty]
+        public RegisterDTO RegisterForm { get; set; }
+
+        public PasswordSet PasswordDTO { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public class PasswordSet
+        {
+            public string OldPassword { get; set; }
+            public string NewPassword { get; set; }
         }
     }
 }
