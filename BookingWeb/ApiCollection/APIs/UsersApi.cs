80aa31d4d853bc0b5579417d0199fd4c451ac927
Magauiya Mazhit <m.mazhit@astanait.edu.kz>
2021-05-12 12:33:44 +0600

    Front-end: login&register pages added with their common layout, added layout for adminka.

--- BookingWeb/ApiCollection/APIs/UsersApi.cs
+++ BookingWeb/ApiCollection/APIs/UsersApi.cs
@@ -1,6 +1,32 @@
-﻿namespace BookingWeb.ApiCollection.APIs
+﻿using BookingWeb.ApiCollection.Infrastructure;
+using BookingWeb.ApiCollection.Interfaces;
+using BookingWeb.ApiCollection.Settings;
+using BookingWeb.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingWeb.ApiCollection.APIs
 {
-    public class UsersApi
+    public class UsersApi : BaseHttpClientWithFactory, IUsersApi
     {
+
+        private IApiSettings _settings;
+        public UsersApi(IHttpClientFactory factory, IApiSettings settings) : base(factory)
+        {
+            _settings = settings;
+            _builder = new HttpRequestBuilder(_settings.BaseAddress);
+            _builder.AddToPath(_settings.UsersPath);
+        }
+
+        public async Task RegisterOwner(RegisterDTO registerForm)
+        {
+            using var message = _builder.AddToPath("register")
+           .HttpMethod(HttpMethod.Post).
+           Content(new StringContent(JsonConvert.SerializeObject(registerForm), Encoding.UTF8, "application/json"))
+           .GetHttpMessage();
+            await SendRequestAsync<RegisterDTO>(message);
+        }
     }
 }
--- BookingWeb/ApiCollection/Interfaces/IUsersApi.cs
+++ BookingWeb/ApiCollection/Interfaces/IUsersApi.cs
@@ -1,6 +1,10 @@
-﻿namespace BookingWeb.ApiCollection.Interfaces
+﻿using BookingWeb.Models;
+using System.Threading.Tasks;
+
+namespace BookingWeb.ApiCollection.Interfaces
 {
     public interface IUsersApi
     {
+        Task RegisterOwner(RegisterDTO registerForm);
     }
 }
